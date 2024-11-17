092f601a0361c36334ec7d7396333ef6c36608e7
Wisommm <wilsondcv711@gmail.com>
2024-11-16 20:17:38 -0500

    Iconos, contacto e info
    Tercer commit

--- app/src/main/java/com/example/cardpresentation/MainActivity.kt
+++ app/src/main/java/com/example/cardpresentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,27 +41,65 @@ fun BusinessCard() {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.android_logo),
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Jennifer Doe",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(
+                text = "Android Developer Extraordinaire",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFF388E3C)
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ContactInfo(icon = R.drawable.ic_phone, text = "+11 (123) 444 555 666")
+            Spacer(modifier = Modifier.height(8.dp))
+            ContactInfo(icon = R.drawable.ic_share, text = "@AndroidDev")
+            Spacer(modifier = Modifier.height(8.dp))
+            ContactInfo(icon = R.drawable.ic_email, text = "jen.doe@android.com")
+        }
+    }
+}
+
+@Composable
+fun ContactInfo(icon: Int, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
             contentDescription = null,
-            modifier = Modifier.size(100.dp),
-            contentScale = ContentScale.Crop
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
         )
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.width(16.dp))
         Text(
-            text = "Jennifer Doe",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-        Text(
-            text = "Android Developer Extraordinaire",
-            fontSize = 16.sp,
+            text = text,
+            fontSize = 14.sp,
             fontWeight = FontWeight.Normal,
-            color = Color(0xFF388E3C)
+            color = Color.Black
         )
     }
 }
