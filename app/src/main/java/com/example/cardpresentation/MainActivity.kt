0746b17a76d15fbb528d840ce4ef984e082b7cab
Wisommm <wilsondcv711@gmail.com>
2024-11-16 20:05:48 -0500

    Primer Commit
    Configuración inicial.

--- app/src/main/java/com/example/cardpresentation/MainActivity.kt
+++ app/src/main/java/com/example/cardpresentation/MainActivity.kt
@@ -7,23 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cardpresentation.ui.theme.CardPresentationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            CardPresentationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CardPresentationTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFFDCE7D8)
+                ) {
+                    BusinessCard()
                 }
             }
         }
@@ -31,17 +32,5 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CardPresentationTheme {
-        Greeting("Android")
-    }
+fun BusinessCard() {
 }
