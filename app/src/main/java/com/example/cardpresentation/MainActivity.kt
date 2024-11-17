922fdd43ab6a3c52b52997898353434bc3929f2f
Wisommm <wilsondcv711@gmail.com>
2024-11-16 20:07:25 -0500

    SecondCommit
    Segundo commit

--- app/src/main/java/com/example/cardpresentation/MainActivity.kt
+++ app/src/main/java/com/example/cardpresentation/MainActivity.kt
@@ -3,23 +3,26 @@ package com.example.cardpresentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cardpresentation.ui.theme.CardPresentationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardPresentationTheme() {
+            CardPresentationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xFFDCE7D8)
@@ -33,4 +36,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = null,
+            modifier = Modifier.size(100.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Jennifer Doe",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Text(
+            text = "Android Developer Extraordinaire",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            color = Color(0xFF388E3C)
+        )
+    }
 }
